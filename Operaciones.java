842f9e26ab21e8d1dbd5485b48c3c739ea2076a4
Pratik <33894580+PANU10@users.noreply.github.com>
2018-03-24 16:27:49 +0100

    Operaciones + - / * %

---
+++ Operaciones.java
@@ -0,0 +1,34 @@
+
+public class Operaciones {
+
+
+    public static void main(String[] args) {
+        
+       int a,b,c;
+       a = 10;
+       b = 10;
+       c = a / b; //Podemos hacer +, -, *, /, 
+       
+       System.out.println(" Valor de a / b són : " +c);
+       
+       double d,e,f;
+       
+       d = 1.5;
+       e = 3.0;
+       f = d+e;
+       
+       System.out.println(" Valor de d + e són: " +f);
+       f = d - e;
+       System.out.println(" Valor de d - e són: " +f);
+       f = d*e;
+       System.out.println(" Valor de d * e són: " +f);
+       f = d / e;
+       System.out.println(" Valor de d / e són: " +f);
+       f = d % e;
+       System.out.println(" Valor de d % e són: " +f);
+   
+       
+      
+    }
+    
+}
