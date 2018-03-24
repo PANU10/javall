bd7cbbc0ab1d425bc3b1b3daa18de136fb1fef3e
Pratik <33894580+PANU10@users.noreply.github.com>
2018-03-24 16:08:47 +0100

    Add files via upload

---
+++ Atributos.java
@@ -0,0 +1,27 @@
+
+public class Atributos {
+
+
+    public static void main(String[] args) {
+        
+        int x; //Entero: 10,20,30.....(no són decimales)
+        x = 100;
+        int y;
+        y = 50;
+        double z;//FLotante: 1.5, 18.5, 5.8 (Són decimales)
+        z = 1.5;
+        char n; //Solo podemos poner un numero o una letra (Ejemplo 1 0 2 0 3, a o b o c o d).
+        n = 'P';
+        
+        System.out.println("Valor de x es:" +x);
+        System.out.println("Valor de y es:" +y);
+        System.out.println("Valor de deciaml:" +z);
+        System.out.println("Valor de x es: " +x+ " ,Valor de z es: " +z);
+        System.out.println("Valor de letra es: " +n);
+        
+ 
+}
+
+    }
+    
+
