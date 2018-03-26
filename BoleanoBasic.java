be4d6124b5a3308034571cc56be6a85021fd76ad
Pratik <33894580+PANU10@users.noreply.github.com>
2018-03-26 15:16:08 +0200

    Boleano true y false;

---
+++ BoleanoBasic.java
@@ -0,0 +1,23 @@
+
+package boleano.basic;
+
+/**
+ *
+ * @author Pratik
+ */
+public class BoleanoBasic {
+
+    public static void main(String[] args) {
+        boolean resultado; // Solo lo podemos declarar con true y false.
+        resultado = true;
+        
+        if (10 < 30){
+            System.out.println(" Es verdad. ");
+        }
+        else{
+        System.out.println(" Es falso. ");
+        }
+        
+                }
+    
+}
